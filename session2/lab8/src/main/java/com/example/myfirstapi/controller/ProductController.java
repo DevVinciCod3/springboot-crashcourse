9a7566ce637a4b09703c759738622513b8fb3acc
Marc Vincent Alcoriza <marcvincent.alcoriza@GXCHLAP00692.local>
2025-07-19 12:11:28 +0800

    Lab 8: Create GET All Products Endpoint

---
+++ session2/lab8/src/main/java/com/example/myfirstapi/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.myfirstapi.controller;
+import com.example.myfirstapi.model.Product;
+import com.example.myfirstapi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+}

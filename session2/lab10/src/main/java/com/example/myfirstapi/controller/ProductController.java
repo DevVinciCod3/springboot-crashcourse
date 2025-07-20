4e672170f16171405d8381d67374d1a8492a0fd0
Marc Vincent Alcoriza <marcvincent.alcoriza@GXCHLAP00692.local>
2025-07-20 15:38:23 +0800

    Lab 10: Create POST Product Endpoint

---
+++ session2/lab10/src/main/java/com/example/myfirstapi/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.example.myfirstapi.controller;
+
+import com.example.myfirstapi.model.Product;
+import com.example.myfirstapi.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/products")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+}
